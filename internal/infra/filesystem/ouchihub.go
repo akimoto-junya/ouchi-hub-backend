6c1caa44a64395eeb25a34822a839a1b1f091a0e
akimoto <akimoto14301@gmail.com>
2024-03-20 16:43:40 +0900

    test(filesystem): mode表示 (#23)

--- internal/infra/filesystem/ouchihub.go
+++ internal/infra/filesystem/ouchihub.go
@@ -19,7 +19,7 @@ func NewOuchiHubStorage() (OuchiHubStorage, error) {
 	path := config.MustGetStoragePath()
 	if d, err := os.Stat(path); err == nil || !d.IsDir() {
 		if err == nil {
-			err = fmt.Errorf("%s is not directory", d.Name())
+			err = fmt.Errorf("%s is not directory, mode (%s)", d.Name(), d.Mode().String())
 		}
 		return nil, fmt.Errorf("%w: %w", ErrStorageNotFound, err)
 	}
